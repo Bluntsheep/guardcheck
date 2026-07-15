944d3b9ee4b34e3f1ce7fae7ed5eb51b4f757613
Bluntsheep <64330238+Bluntsheep@users.noreply.github.com>
2026-07-15 16:31:06 +0200

    Update database connection to use environment variables

--- app/api/admin/accounts/route.js
+++ app/api/admin/accounts/route.js
@@ -2,11 +2,11 @@ import { NextResponse } from "next/server";
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "sql6.jnb1.host-h.net",
-  user: "puzzled_user",
-  password: "z01W079712LSp7",
-  database: "puzzled_new_database",
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
