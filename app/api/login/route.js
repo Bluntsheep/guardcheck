5b1821d26db53206866cca91ac38a576ca9f70ca
Bluntsheep <64330238+Bluntsheep@users.noreply.github.com>
2026-07-15 06:04:14 +0200

    Change database name to use environment variable

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "puzzled_new_database",
+  database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
